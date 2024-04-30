2986310d44f16f5411d1f9110bf3de11d0d7447d
Michael Connell <connellmp@gmail.com>
2024-04-30 11:43:08 -0700

    add sqlfluff and format files

--- models/marts/fact_nodes.sql
+++ models/marts/fact_nodes.sql
@@ -1,50 +1,53 @@
-with sources as (
-    select
-        command_invocation_id,
-        node_id,
-        run_started_at,
-        database_name,
-        schema_name,
-        source_name,
-        loader,
-        name,
-        identifier,
-        loaded_at_field,
-        freshness
-    from {{ ref('stg_source') }}
-),
-seeds as (
-    select
-        command_invocation_id,
-        node_id,
-        run_started_at,
-        database_name,
-        schema_name,
-        name,
-        package_name,
-        path,
-        checksum
-    from {{ ref('stg_seed') }}
-),
-models as (
-    select
-        command_invocation_id,
-        node_id,
-        run_started_at,
-        database_name,
-        schema_name,
-        name,
-        depends_on_nodes,
-        package_name,
-        path,
-        checksum,
-        materialization,
-        tags,
-        meta,
-        description,
-        total_rowcount
-    from {{ ref('stg_model') }}
-)
+with
+    sources as (
+        select
+            command_invocation_id,
+            node_id,
+            run_started_at,
+            database_name,
+            schema_name,
+            source_name,
+            loader,
+            name,
+            identifier,
+            loaded_at_field,
+            freshness
+        from {{ ref('stg_source') }}
+    ),
+
+    seeds as (
+        select
+            command_invocation_id,
+            node_id,
+            run_started_at,
+            database_name,
+            schema_name,
+            name,
+            package_name,
+            path,
+            checksum
+        from {{ ref('stg_seed') }}
+    ),
+
+    models as (
+        select
+            command_invocation_id,
+            node_id,
+            run_started_at,
+            database_name,
+            schema_name,
+            name,
+            depends_on_nodes,
+            package_name,
+            path,
+            checksum,
+            materialization,
+            tags,
+            meta,
+            description,
+            total_rowcount
+        from {{ ref('stg_model') }}
+    )
 
 select 1
 -- hoping to create foreign keys between
