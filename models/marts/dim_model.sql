a779d70b6d730b41c1879e175b2f6016c5615596
Michael Connell <connellmp@gmail.com>
2024-05-01 14:17:37 -0700

    add is_tested column

--- models/marts/dim_model.sql
+++ models/marts/dim_model.sql
@@ -1,37 +1,44 @@
-with model as (
-    select
-        command_invocation_id,
-        node_id,
-        database_name,
-        schema_name,
-        name,
-        package_name,
-        path,
-        checksum,
-        materialization,
-        tags,
-        meta,
-        description
-    from {{ ref('stg_model') }}
-)
+with
+    model as (
+        select
+            {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as model_key,
+            node_id,
+            database_name,
+            schema_name,
+            name,
+            package_name,
+            path,
+            checksum,
+            materialization,
+            tags,
+            meta,
+            description
+        from {{ ref('stg_model') }}
+    ),
 
-select
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as model_key,
-    node_id,
-    database_name,
-    schema_name,
-    name,
-    package_name,
-    path,
-    checksum,
-    materialization,
-    tags,
-    meta,
-    description
-from model
+    _tests as (
+        select lower(depends_on_nodes) as depends_on_nodes
+        from {{ ref('stg_test') }}
+    ),
 
-/*
-should dim_model just be models, or all invocations? ie dim_resource
-with all resources and invocation history and then dim_model
-with just node, name, database, schema, etc
-*/
+    _models as (
+        select
+            '%' || lower(node_id) || '%' as node_key,
+            node_id,
+            model_key
+        from model
+    ),
+
+    untested_models as (
+        select distinct model.model_key
+        from _models as model
+        left outer join _tests as test
+            on test.depends_on_nodes like model.node_key
+        where test.depends_on_nodes is null
+    ),
+
+    final as (
+        select model.*, case when untested_models.model_key is null then 1 else 0 end as is_tested from model left join untested_models on model.model_key = untested_models.model_key
+    )
+
+select * from final
