ee9553cf5c518d102a3f0b12e7ae74063bbe0feb
Michael Connell <connellmp@gmail.com>
2024-05-08 15:27:33 -0700

    update execution dim and meta

--- models/marts/dim_execution.sql
+++ models/marts/dim_execution.sql
@@ -2,14 +2,14 @@ with
     executions as (
         select
             command_invocation_id,
-            node_id,
+            resource_type,
+            project,
+            resource_name,
             status,
             materialization,
             schema_name,
-            name,
-            run_started_at,
-            resource_type
-        from {{ ref('stg_execution') }}
+            run_started_at
+        from {{ ref('int_execution') }}
     ),
 
     most_recent as (
@@ -18,12 +18,12 @@ with
     )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['executions.command_invocation_id', 'executions.node_id']) }} as execution_key,
-    executions.node_id,
+    {{ dbt_utils.generate_surrogate_key(['executions.command_invocation_id', 'executions.resource_type', 'executions.project', 'executions.resource_name']) }} as execution_key,
+    executions.resource_type,
+    executions.project,
+    executions.resource_name,
     executions.status,
     executions.materialization,
     executions.schema_name,
-    executions.name,
-    executions.resource_type,
     case when executions.run_started_at = most_recent.most_recent_run then 1 else 0 end as most_recent_run
 from executions left join most_recent on executions.run_started_at = most_recent.most_recent_run
