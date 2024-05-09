f7596223d51e0ec8333afc09610f6209903b3549
Michael Connell <connellmp@gmail.com>
2024-05-09 15:15:57 -0700

    add meta labels for dim_model

---
+++ models/intermediate/int_model.sql
@@ -0,0 +1,20 @@
+select
+    command_invocation_id,
+    node_id,
+    {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=1) }} as resource_type,
+    {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=2) }} as project,
+    {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=3) }} as resource_name,
+    run_started_at,
+    database_name,
+    schema_name,
+    name,
+    depends_on_nodes,
+    package_name,
+    path,
+    checksum,
+    materialization,
+    tags,
+    meta,
+    description,
+    total_rowcount
+from {{ ref('models') }}
--- models/marts/dim_model.sql
+++ models/marts/dim_model.sql
@@ -1,8 +1,13 @@
 with
-    model as (
+    models as (
         select
-            {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as model_key,
+            {{ dbt_utils.generate_surrogate_key([
+                'command_invocation_id', 'resource_type', 'project', 'resource_name'
+                ]) }} as model_key,
             node_id,
+            resource_type,
+            project,
+            resource_name,
             database_name,
             schema_name,
             name,
@@ -13,7 +18,7 @@ with
             tags,
             meta,
             description
-        from {{ ref('stg_model') }}
+        from {{ ref('int_model') }}
     ),
 
     _tests as (
@@ -26,23 +31,36 @@ with
             '%' || lower(node_id) || '%' as node_key,
             node_id,
             model_key
-        from model
+        from models
     ),
 
     untested_models as (
         select distinct model.model_key
-        from _models as model
+        from _models as models
         left outer join _tests as test
-            on test.depends_on_nodes like model.node_key
+            on test.depends_on_nodes like models.node_key
         where test.depends_on_nodes is null
     ),
 
     final as (
         select
-            model.*,
-            case when untested_models.model_key is null then 1 else 0 end as is_tested
-        from model
-        left outer join untested_models on model.model_key = untested_models.model_key
+            models.model_key,
+            models.resource_type,
+            models.project,
+            models.resource_name,
+            models.database_name,
+            models.schema_name,
+            models.name,
+            models.package_name,
+            models.path,
+            models.checksum,
+            models.materialization,
+            models.tags,
+            models.meta,
+            models.description,
+            case when untested_models.model_key is null then 'Yes' else 'No' end as is_tested
+        from models
+        left outer join untested_models on models.model_key = untested_models.model_key
     )
 
 select * from final
