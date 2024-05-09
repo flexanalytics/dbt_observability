6371573b931383ecaa755ab3c137079530f87cba
Michael Connell <connellmp@gmail.com>
2024-05-09 15:21:32 -0700

    add meta labels for dim_seed

--- models/marts/dim_seed.sql
+++ models/marts/dim_seed.sql
@@ -2,19 +2,28 @@ with
     seeds as (
         select
             command_invocation_id,
-            node_id,
+            resource_type,
+            project,
+            resource_name,
             database_name,
             schema_name,
             name,
             package_name,
             path,
             checksum
-        from {{ ref('stg_seed') }}
+        from {{ ref('int_seed') }}
     )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as seed_key,
-    node_id,
+    {{ dbt_utils.generate_surrogate_key([
+        'command_invocation_id',
+        'resource_type',
+        'project',
+        'resource_name']
+        ) }} as seed_key,
+    resource_type,
+    project,
+    resource_name,
     database_name,
     schema_name,
     name,
