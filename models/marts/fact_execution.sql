f7a362ebeed6ff00f8d295c2a7a58a02a17fff59
Michael Connell <connellmp@gmail.com>
2024-05-13 11:14:01 -0700

    update surrogate key logic

--- models/marts/fact_execution.sql
+++ models/marts/fact_execution.sql
@@ -3,6 +3,9 @@ with
         select
             command_invocation_id,
             node_id,
+            resource_type,
+            project,
+            resource_name,
             run_started_at,
             cast(
                 {{
@@ -18,22 +21,20 @@ with
             query_completed_at,
             total_node_runtime,
             materialization,
-            schema_name,
-            name,
-            resource_type
-        from {{ ref('stg_execution') }}
+            schema_name
+        from {{ ref('int_execution') }}
     )
 select
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as execution_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as execution_key,
     {{ dbt_utils.generate_surrogate_key(['command_invocation_id']) }} as invocation_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as model_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as column_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as test_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as seed_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as source_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as snapshot_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as metric_key,
-    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as exposure_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as model_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as column_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as test_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as seed_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as source_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as snapshot_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as metric_key,
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'resource_type', 'project', 'resource_name']) }} as exposure_key,
     {{ dbt_utils.generate_surrogate_key(['run_start_day']) }} as date_key,
     run_started_at,
     was_full_refresh,
