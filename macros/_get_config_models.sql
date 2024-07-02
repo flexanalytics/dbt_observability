5a3a888f459ef6672ba5182026b1c5c0bfb2c721
Michael Connell <connellmp@gmail.com>
2024-07-02 13:03:44 -0700

    default materialization to tables and allow array of materializations

--- macros/_get_config_models.sql
+++ macros/_get_config_models.sql
@@ -1,4 +1,4 @@
-{% macro get_models_list(graph, path=None, materialization=None) %}
+{% macro get_models_list(graph, path=None, materialization=[]) %}
     {% set models = [] %}
         {% for node in graph.nodes.values() | selectattr("resource_type", "equalto", "model") | selectattr("package_name", "equalto", project_name) %}
             {% if path and not materialization %}
@@ -6,11 +6,11 @@
                     {% do models.append(node) %}
                 {% endif %}
             {% elif not path and materialization %}
-                {% if node.config.materialized == materialization %}
+                {% if node.config.materialized in materialization %}
                     {% do models.append(node) %}
                 {% endif %}
             {% elif path %}
-                {% if (path + node.name + ".sql") == node.original_file_path and node.config.materialized == materialization %}
+                {% if (path + node.name + ".sql") == node.original_file_path and node.config.materialized in materialization %}
                     {% do models.append(node) %}
                 {% endif %}
             {% else %}
