023d99cbc13ef07b7e9a1ed6e854943948e5c71c
Andrew Taft <11670864+ataft@users.noreply.github.com>
2024-08-07 00:41:51 -0700

    Create no_anomoly.sql
    Initial iteration on `no_anomoly` generic test

---
+++ macros/no_anomoly.sql
@@ -0,0 +1,27 @@
+{% test no_anomoly(model) %}
+
+with
+    hist as (
+        select avg(total_rowcount) as average_rowcount
+        from {{ ref('sources') }} -- {{ database }}.{{ target.schema }}_observability.sources
+        where (database_name || '.' || source_name || '.' || identifier) = '{{ model }}'
+            and total_rowcount is not null
+    ),
+
+    curr as (
+        select count(*) as current_rowcount
+        from {{ model }}
+    )
+
+select
+    curr.current_rowcount,
+    hist.average_rowcount,
+    curr.current_rowcount - coalesce(hist.average_rowcount, 0) as rowcount_diff,
+    cast(rowcount_diff as numeric(19, 6)) / cast(curr.current_rowcount as numeric(19, 6)) as rowcount_diff_pct
+from curr
+left outer join hist
+    on 1 = 1
+where rowcount_diff_pct > .05 -- TODO: pass as parameter
+    or rowcount_diff_pct < -.05
+
+{% endtest %}
