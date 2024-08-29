3efbd12df039c711db75b8125b7d4e0deaacf43a
Andrew Taft <11670864+ataft@users.noreply.github.com>
2024-08-29 07:00:52 -0700

    File Path Fix
    Replace Windows path style (e.g. \, but escaped as \\) with desired/universal path style (e.g. /).

--- macros/_get_config_models.sql
+++ macros/_get_config_models.sql
@@ -1,8 +1,9 @@
 {% macro get_models_list(graph, path=None, materialization=[]) %}
     {% set models = [] %}
         {% for node in graph.nodes.values() | selectattr("resource_type", "equalto", "model") | selectattr("package_name", "equalto", project_name) %}
+            {% set orig_path = (node.original_file_path | replace('\\', '/')) %}
             {% if path and not materialization %}
-                {% if (path + node.name + ".sql") == node.original_file_path %}
+                {% if (path + node.name + ".sql") == orig_path %}
                     {% do models.append(node) %}
                 {% endif %}
             {% elif not path and materialization %}
@@ -10,7 +11,7 @@
                     {% do models.append(node) %}
                 {% endif %}
             {% elif path %}
-                {% if (path + node.name + ".sql") == node.original_file_path and node.config.materialized in materialization %}
+                {% if (path + node.name + ".sql") == orig_path and node.config.materialized in materialization %}
                     {% do models.append(node) %}
                 {% endif %}
             {% else %}
